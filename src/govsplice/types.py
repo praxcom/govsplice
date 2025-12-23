5cd0f93317d3ce2b6e24baaffc6f9bccc5c8bd5a
praxcom <208060600+praxcom@users.noreply.github.com>
2025-12-23 10:30:49 +0000

    repo: Delete redundant files

--- src/govsplice/types.py
+++
@@ -1,7 +0,0 @@
-# /src/govsplice/types.py
-"""This module defines custom types for hinting use in the Govsplice project."""
-
-from typing import AsyncGenerator, Any
-
-GeoJSON = dict[str, Any]
-JSON = dict[str, Any]
